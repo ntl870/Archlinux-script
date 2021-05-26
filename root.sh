26a1f7f7a47c5dd3ac24e68a1c776b20ab575f1c
ntl870 <ntlong870@gmail.com>
2021-05-26 21:18:44 +0700

    updated

--- root.sh
+++ root.sh
@@ -7,8 +7,8 @@ mkdir /mnt
 mkdir /mnt/home
 mkdir /mnt/boot
 mkdir /mnt/boot/EFI
-mkdir /etc
-mkdir /etc/fstab
+mkdir /mnt/etc
+mkdir /mnt/etc/fstab
 echo "genfstab -U /mnt >> /mnt/etc/fstab"
 echo "pacstrap -i /mnt base base-devel"
 echo "arch-chroot /mnt"
