86d0f1bcf5b4cbb80ee8bb21166a52dc138f51da
ntl870 <ntlong870@gmail.com>
2021-05-27 18:36:53 +0700

    updated

--- post_tweaks.sh
+++ post_tweaks.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo timedatectl set-timezone Asia/Ho_Chi_Minh
-sudo systemctl enable sysdemd-timesyncd
+sudo systemctl enable systemd-timesyncd
 echo "Enter hostname"
 read hostname
 sudo hostnamectl set-hostname $hostname
 basehost="127.0.0.1 "
 echo "127.0.0.1 localhost" >>/etc/hosts
 echo "$basehost$hostname" >>/etc/hosts
-sudo pacman -S --noconfirm intel-ucode xorg xorg-server nvidia-lts nvidia mesa
+sudo pacman -S --noconfirm intel-ucode xorg xorg-server nvidia-lts nvidia mesa linux-firmware
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
